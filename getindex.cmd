3be47224297e61b749dcd63c484a450863ace077
Matthew <matthewcocks@hotmail.com>
2020-06-15 22:33:04 +0100

    Serve many artifacts

--- getindex.cmd
+++ getindex.cmd
@@ -1,2 +1,7 @@
 
 curl -L -o ./public/index.html https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb.html
+
+curl -L -o ./public/The-Return-of-Tom-Thumb.epub https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb.epub
+
+curl -L -o ./public/The-Return-of-Tom-Thumb-with-music.mp3 https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb-with-music.mp3
+
