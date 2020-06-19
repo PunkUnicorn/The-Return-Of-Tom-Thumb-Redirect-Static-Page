2cb1db6f4eaa79e9de8a9dd2bdc6dafbe4c0f421
Matthew <matthewcocks@hotmail.com>
2020-06-20 00:47:20 +0100

    Ready to release more chapters

--- getindex.cmd
+++ getindex.cmd
@@ -1,7 +1,7 @@
 
-curl -L -o ./public/index.html https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb.html
+curl -L -o ./public/index.html https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb-single-chapter-one.html
 
-curl -L -o ./public/The-Return-of-Tom-Thumb.epub https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb.epub
+curl -L -o ./public/The-Return-of-Tom-Thumb-single-chapter-one.epub https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb-single-chapter-one.epub
 
-curl -L -o ./public/The-Return-of-Tom-Thumb-with-music.mp3 https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb-with-music.mp3
+curl -L -o ./public/The-Return-of-Tom-Thumb-with-music-single-chapter-one.mp3 https://ci.appveyor.com/api/projects/PunkUnicorn/the-return-of-tom-thumb/artifacts/The-Return-of-Tom-Thumb-with-music-single-chapter-one.mp3
 
